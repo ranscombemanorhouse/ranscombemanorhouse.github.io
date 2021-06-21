1a555744a190e1cf710698377cd9a4570c04c00b
True Ghiassi <true@ghiassitrio.co.uk>
2021-06-21 13:54:06 +0100

    Polished up images in slideshow, and added some more. Fixed font flash (again) and handled wait slideshow image loading.

--- elmSlideshow/src/ManualSlideshow.elm
+++ elmSlideshow/src/ManualSlideshow.elm
@@ -1,4 +1,4 @@
-module ManualSlideshow exposing (main)
+port module ManualSlideshow exposing (main)
 
 import Array
 import Browser
@@ -9,6 +9,9 @@ import Element.Font as Font
 import Element.Input as Ei
 import Html
 import Html.Attributes as Hat
+
+
+port pageloaded : (Int -> msg) -> Sub msg
 
 
 main =
@@ -27,11 +30,12 @@ type Msg
     | OverRightButton
     | LeaveLeftButton
     | LeaveRightButton
+    | PageLoaded Int
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { position = 0, over = NotOver }, Cmd.none )
+    ( { position = 0, over = NotOver, loaded = False }, Cmd.none )
 
 
 view : Model -> Html.Html Msg
@@ -42,25 +46,9 @@ view model =
 images : Array.Array ( String, String )
 images =
     Array.fromList
-        [ ( "18.jpg", "Ornamental parterre in front of house" )
+        [ ( "44.jpg", "Arial view of house and surroundings" )
+        , ( "45.jpg", "High arial view, including distant sea view" )
         , ( "ranscombe3dView.jpg", "3D view of Ranscombe interior" )
-        , ( "07.jpg", "Wisteria bedroom" )
-        , ( "08.jpg", "Wisteria bedroom" )
-        , ( "10.jpg", "View from Rose bathroom window" )
-        , ( "11.jpg", "Sitting room" )
-        , ( "12.jpg", "Dining room" )
-        , ( "13.jpg", "View of parterre from front door" )
-        , ( "14.jpg", "Front porch" )
-        , ( "15.jpg", "Front of house" )
-        , ( "16.jpg", "Builder's engraving on stone" )
-        , ( "17.jpg", "Seating area outside front of house" )
-        , ( "19.jpg", "View of house from behind barns" )
-        , ( "20.jpg", "Arial view of house and barns" )
-        , ( "21.jpg", "Arial view of house and surroundings" )
-        , ( "23.jpg", "View of nearby fields" )
-        , ( "24.jpg", "Pink flower on iron railing" )
-        , ( "25.jpg", "Pigeon flying from fountain" )
-        , ( "26.jpg", "View of pond" )
         , ( "27.jpg", "Swings on lawn" )
         , ( "28.jpg", "Picnic by the pond" )
         , ( "29.jpg", "Stove in kitchen and living area" )
@@ -71,22 +59,41 @@ images =
         , ( "34.jpg", "Upstairs hallway" )
         , ( "35.jpg", "Rose bathroom" )
         , ( "36.jpg", "Rose bedroom" )
-        , ( "03.jpg", "Fern bedroom" )
         , ( "37.jpg", "Ivy bedroom" )
         , ( "38.jpg", "Ivy bathroom" )
         , ( "39.jpg", "Wisteria bedroom" )
         , ( "40.jpg", "Wisteria bathroom" )
-        , ( "41.jpg", "Sofas in kitchen and living area" )
+        , ( "41.jpg", "Kitchen and living area" )
         , ( "42.jpg", "Sitting room" )
         , ( "43.jpg", "Kitchen and living area" )
-        , ( "44.jpg", "Arial view of house and surroundings" )
-        , ( "45.jpg", "High arial view, including distant sea view" )
+        , ( "085117.jpg", "Cottage living area" )
+        , ( "085149.jpg", "Cottage living area" )
+        , ( "085451.jpg", "Cottage conservatory" )
+        , ( "204133.jpg", "Ranscombe front porch" )
+        , ( "18.jpg", "Ornamental parterre in front of the Manor" )
+        , ( "10.jpg", "View from Rose bathroom window" )
+        , ( "11.jpg", "Sitting room" )
+        , ( "12.jpg", "Dining room" )
+        , ( "13.jpg", "View from front door" )
+        , ( "14.jpg", "Front porch" )
+        , ( "15.jpg", "Front of house" )
+        , ( "16.jpg", "Builder's engraving on stone" )
+        , ( "17.jpg", "Seating area in front of house" )
+        , ( "19.jpg", "View of house from behind barns" )
+        , ( "20.jpg", "Arial view of house and barns" )
+        , ( "21.jpg", "Arial view of house and surroundings" )
+        , ( "23.jpg", "View of nearby fields" )
+        , ( "24.jpg", "Pink flower on iron railing" )
+        , ( "25.jpg", "Pigeon flying from fountain" )
+        , ( "26.jpg", "View of pond" )
+        , ( "03.jpg", "Fern bedroom" )
         ]
 
 
 type alias Model =
     { position : Int
     , over : Over
+    , loaded : Bool
     }
 
 
@@ -103,20 +110,48 @@ viewHelp model =
             E.none
 
         Just ( root, alt ) ->
-            oneSlide model.position root alt model.over
+            oneSlide model.loaded model.position root alt model.over
 
 
-oneSlide : Int -> String -> String -> Over -> E.Element Msg
-oneSlide counter root alt over =
-    E.column [] <|
-        [ E.row
-            [ E.width E.fill ]
+oneSlide : Bool -> Int -> String -> String -> Over -> E.Element Msg
+oneSlide loaded counter root alt over =
+    E.column [E.width E.fill, E.spacing 6] <|
+        [ if loaded then
+            E.el [ E.width E.fill ] (image root alt)
+          else
+            E.el [ E.width E.fill ] <|
+                case Array.get 1 images of
+                    Nothing ->
+                        E.none
+
+                    Just (root1, alt1) ->
+                        image root1 alt1
+            
+        , E.row [E.width E.fill, E.spacing 6]
             [ leftButton over
-            , image root alt
             , rightButton over
             ]
-        , counterView counter
+        , if loaded then
+            caption alt
+          else
+            E.el captionStyle (E.text "Loading...")
+        , if loaded then
+            counterView counter
+          else
+            E.none
         ]
+
+
+captionStyle =
+    [ Font.family [ Font.typeface "Alegreya" ]
+    , Font.size 20
+    , E.centerX
+    , E.padding 5
+    ]
+
+
+caption alt =
+    E.el captionStyle (E.text alt)
 
 
 counterView counter =
@@ -133,14 +168,15 @@ counterView counter =
 
 
 buttonTextStyle =
-    [ Font.size 80
-    , Font.color <| E.rgb255 220 220 220
+    [ Font.size 100
+    , Font.color <| E.rgb255 180 180 180
+    , E.centerX
     ]
 
 
 buttonStyle =
-    [ E.height E.fill
-    , noOutline
+    [ noOutline
+    , E.width E.fill
     ]
 
 
@@ -165,16 +201,16 @@ leftButton over =
         , label =
             E.el
                 buttonTextStyle
-                (E.text "<")
+                (E.text "<<")
         }
 
 
 hover =
-    E.rgb255 240 240 240
+    E.rgb255 215 215 215
 
 
 notHover =
-    E.rgb255 255 255 255
+    E.rgb255 230 230 230
 
 
 rightButton over =
@@ -194,7 +230,7 @@ rightButton over =
         , label =
             E.el
                 buttonTextStyle
-                (E.text ">")
+                (E.text ">>")
         }
 
 
@@ -209,29 +245,35 @@ image root alt =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Sub.none
+    pageloaded PageLoaded
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         LeftButtonClick ->
-            ( if model.position == 0 then
-                { model | position = Array.length images - 1 }
+            if model.loaded then
+                ( if model.position == 0 then
+                    { model | position = Array.length images - 1 }
 
-              else
-                { model | position = model.position - 1 }
-            , Cmd.none
-            )
+                  else
+                    { model | position = model.position - 1 }
+                , Cmd.none
+                )
+            else
+                (model, Cmd.none)
 
         RightButtonClick ->
-            ( if model.position == Array.length images - 1 then
-                { model | position = 0 }
+            if model.loaded then
+                ( if model.position == Array.length images - 1 then
+                    { model | position = 0 }
 
-              else
-                { model | position = model.position + 1 }
-            , Cmd.none
-            )
+                  else
+                    { model | position = model.position + 1 }
+                , Cmd.none
+                )
+            else
+                (model, Cmd.none)
 
         OverLeftButton ->
             ( { model | over = OverLeft }, Cmd.none )
@@ -244,3 +286,6 @@ update msg model =
 
         LeaveRightButton ->
             ( { model | over = NotOver }, Cmd.none )
+
+        PageLoaded _ ->
+            ( { model | loaded = True } , Cmd.none )
